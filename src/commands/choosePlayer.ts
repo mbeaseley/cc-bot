5aeea51805ba204e2690ecbaca9180587a176dad
mbeaseley <m.t.beaseley@gmail.com>
2021-02-21 19:07:20 +0000

    optimize for any voice channel

--- src/commands/choosePlayer.ts
+++ src/commands/choosePlayer.ts
@@ -1,14 +1,33 @@
 import { CommandMessage } from '@typeit/discord';
-import { GuildChannel } from 'discord.js';
+import { GuildChannel, User } from 'discord.js';
 
 export class ChoosePlayer {
-  private getChannel(
-    command: CommandMessage,
-    id: string
-  ): GuildChannel | undefined {
-    return command?.guild?.channels?.cache.get(id);
+  /**
+   * Get Author of command
+   * @param command
+   */
+  getAuthor(command: CommandMessage): User {
+    return command?.author;
   }
 
+  private findUserChannel(command: CommandMessage): GuildChannel | undefined {
+    const voiceChannels = command?.guild?.channels?.cache?.filter(
+      (c) => c.type === 'voice'
+    );
+
+    const author = this.getAuthor(command);
+
+    const channel = voiceChannels?.find(
+      (c) => !!c.members.find((m) => m.id === author.id)
+    );
+
+    return channel;
+  }
+
+  /**
+   * Get unique username from channel
+   * @param channel
+   */
   private getUsers(channel: GuildChannel | undefined): string[] {
     const users = channel?.guild?.members?.cache.map((x) => {
       if (!x.user?.bot) {
@@ -27,8 +46,12 @@ export class ChoosePlayer {
       });
   }
 
+  /**
+   * Init for player choice
+   * @param command
+   */
   public init(command: CommandMessage): Promise<string> {
-    const channel = this.getChannel(command, '787770844066611200');
+    const channel = this.findUserChannel(command);
 
     const users = this.getUsers(channel);
 
