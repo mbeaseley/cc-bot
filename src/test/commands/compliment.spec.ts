b76af595e29f6ae4cee97abd01b719de26f4481b
mbeaseley <m.t.beaseley@gmail.com>
2021-03-20 04:50:31 +0000

    compliment command unit

--- src/test/commands/compliment.spec.ts
+++ src/test/commands/compliment.spec.ts
@@ -1,4 +1,5 @@
 import { Compliment } from '../../commands/compliment';
+import { command } from '../utils/command';
 
 describe('Compliment', () => {
   let compliment: Compliment;
@@ -7,7 +8,42 @@ describe('Compliment', () => {
     compliment = new Compliment();
   });
 
-  it('initialize', () => {
+  it('Initialize', () => {
     expect(compliment).toBeTruthy();
+  });
+
+  it('Creates compliment and replies', () => {
+    compliment.instance.get = jest
+      .fn()
+      .mockResolvedValue({ compliment: 'Hello World!' });
+
+    const res = command('compliment');
+    const spy = jest.spyOn(res, 'reply');
+
+    return compliment.init(res).then(() => {
+      expect(spy).toHaveBeenCalledWith('Hello World!');
+    });
+  });
+
+  it('Creates insult and sends', () => {
+    compliment.instance.get = jest
+      .fn()
+      .mockResolvedValue({ compliment: 'Hello World!' });
+
+    const res = command('compliment', '<!@>');
+    res.channel.send = jest.fn();
+    const spy = jest.spyOn(res.channel, 'send');
+
+    return compliment.init(res).then(() => {
+      expect(spy).toHaveBeenCalledWith('<!@>, Hello World!');
+    });
+  });
+
+  it('Get empty compliment string', () => {
+    compliment.instance.get = jest.fn().mockResolvedValue(undefined);
+
+    return compliment.init(command('compliment')).catch((res) => {
+      expect(res).toBeUndefined();
+    });
   });
 });
