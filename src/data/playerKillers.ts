64e380dfd24fec1cc3526f1604fdc5e09f65dd11
mbeaseley <m.t.beaseley@gmail.com>
2021-04-09 00:13:59 +0100

    dbd player matrix

---
+++ src/data/playerKillers.ts
@@ -0,0 +1,51 @@
+class PlayerKiller {
+  userId: string | undefined;
+  availableKiller: number[] = [];
+}
+
+export const defaultKillers: number[] = [1, 2, 3, 4, 6, 7, 8];
+
+export const playerKillers: PlayerKiller[] = [
+  {
+    userId: '221073387177508864',
+    availableKiller: [
+      ...defaultKillers,
+      5,
+      9,
+      10,
+      11,
+      12,
+      13,
+      14,
+      15,
+      16,
+      17,
+      18,
+      19,
+      20,
+      21,
+      22,
+    ],
+  },
+  {
+    userId: '147110059799871490',
+    availableKiller: [
+      ...defaultKillers,
+      5,
+      9,
+      10,
+      11,
+      12,
+      13,
+      14,
+      15,
+      16,
+      17,
+      18,
+      19,
+      20,
+      21,
+      22,
+    ],
+  },
+];
