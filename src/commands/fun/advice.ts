fd6c75ba54eda70818ac02710e88b805825916b6
Michael Beaseley <32479825+mbeaseley@users.noreply.github.com>
2021-07-07 13:16:04 +0100

    [DEV] Advice Command (#47)

---
+++ src/commands/fun/advice.ts
@@ -0,0 +1,54 @@
+import { Command, CommandMessage, Description } from '@typeit/discord';
+import { Message, MessageEmbed } from 'discord.js';
+import { Logger } from '../../services/logger.service';
+import { AdviceService } from '../../services/advice.service';
+import Utility from '../../utils/utility';
+
+export class Advice {
+  private logger: Logger;
+  private adviceService: AdviceService;
+
+  constructor() {
+    this.logger = new Logger();
+    this.adviceService = new AdviceService();
+  }
+
+  /**
+   * Display friendly advice
+   * @param command
+   */
+  @Command('advice')
+  @Description('Send some friendly advice to yourself or a friend')
+  async init(command: CommandMessage): Promise<Message> {
+    try {
+      await command.delete();
+
+      const user = Utility.getOptionFromCommand(command.content, 2)?.[0];
+      const givenAdvice = await this.adviceService.getAdvice();
+
+      if (!givenAdvice?.advice) {
+        return command.channel
+          .send('**No advice was given!**')
+          .then((m) => m.delete({ timeout: 5000 }));
+      }
+
+      if (user) {
+        return command.channel.send(`${user}, ${givenAdvice.advice}`);
+      } else {
+        return command.reply(givenAdvice.advice);
+      }
+    } catch (e: unknown) {
+      await command.delete();
+      this.logger.error(
+        `Command: 'advice' has error: ${(e as Error).message}.`
+      );
+      return command.channel
+        .send(
+          `The following error has occurred: ${
+            (e as Error).message
+          }. If this error keeps occurring, please contact support.`
+        )
+        .then((m) => m.delete({ timeout: 5000 }));
+    }
+  }
+}
---
+++ src/models/advice-model.service.ts
@@ -0,0 +1,43 @@
+import { AxiosResponse } from 'axios';
+import { HttpClient } from '../interceptor/httpClient';
+import { ApiAdviceResponse, AdviceItem } from '../types/advice';
+
+export class AdviceModelService extends HttpClient {
+  constructor() {
+    super('');
+  }
+
+  /**
+   * Format into correct type
+   * @param res
+   * @returns AdviceItem
+   */
+  private fromPayload(res: ApiAdviceResponse): AdviceItem | undefined {
+    const slip = res.slip;
+    if (slip) {
+      return new AdviceItem(slip.id, slip.advice);
+    } else {
+      return undefined;
+    }
+  }
+
+  /**
+   * Fetch advice
+   * @returns Promise<AxiosResponse<ApiAdviceResponse>>
+   */
+  private getResponse = (): Promise<AxiosResponse<ApiAdviceResponse>> =>
+    this.instance.get('https://api.adviceslip.com/advice', {
+      headers: {
+        Accept: 'application/json',
+      },
+    });
+
+  /**
+   * Get single advice
+   * @returns Promise<AdviceItem | undefined>
+   */
+  public async getAdvice(): Promise<AdviceItem | undefined> {
+    const res = await this.getResponse();
+    return this.fromPayload(res);
+  }
+}
