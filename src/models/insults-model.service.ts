fef4fdb35b1b761fc418b13758a7ee076b82ceb5
mbeaseley <m.t.beaseley@gmail.com>
2021-07-15 19:26:13 +0100

    insult command refactor

---
+++ src/models/insults-model.service.ts
@@ -0,0 +1,27 @@
+import { AxiosResponse } from 'axios';
+import { HttpClient } from 'Interceptor/httpClient';
+
+export class InsultsModelService extends HttpClient {
+  constructor() {
+    super('https://insult.mattbas.org/api');
+  }
+
+  /**
+   * Fetch Insult
+   * @returns Promise<AxiosResponse<ComplimentObject>>
+   */
+  private getResponse = (): Promise<AxiosResponse<string>> =>
+    this.instance.get('insult', {
+      headers: {
+        Accept: 'application/json',
+      },
+    });
+
+  /**
+   * Gets formatted response
+   * @returns Promise<string>
+   */
+  public async getInsult(): Promise<string> {
+    return this.getResponse();
+  }
+}
