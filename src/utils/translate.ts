d9827e3a59c6d8c1ee90f10d4a4349221cfaef54
mbeaseley <m.t.beaseley@gmail.com>
2021-09-28 16:14:31 +0100

    get all copy

--- src/utils/translate.ts
+++ src/utils/translate.ts
@@ -1,9 +1,30 @@
 import { admin } from 'Language/admin';
 import { error } from 'Language/error';
+import { fun } from 'Language/fun';
+import { games } from 'Language/games';
+import { image } from 'Language/image';
+import { misc } from 'Language/misc';
+import { music } from 'Language/music';
+import { searchers } from 'Language/searchers';
 
 export default class Translate {
+  /**
+   * Find correct translate copy
+   * @param key
+   * @param args
+   * @returns string
+   */
   static find(key: string, args?: string[]): string {
-    const allCopys = { ...admin, ...error };
+    const allCopys = {
+      ...admin,
+      ...error,
+      ...fun,
+      ...games,
+      ...image,
+      ...misc,
+      ...music,
+      ...searchers,
+    };
     const selectedKey = Object.keys(allCopys).find((k) => k === key);
 
     if (!selectedKey) {
