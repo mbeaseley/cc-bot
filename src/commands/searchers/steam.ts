7b477c7a31f8eba397e7e271a454de0e953f8422
mbeaseley <m.t.beaseley@gmail.com>
2021-11-14 21:10:07 +0000

    steam command rewrite

---
+++ src/commands/searchers/steam.ts
@@ -0,0 +1,74 @@
+import { CommandInteraction, MessageEmbed } from 'discord.js';
+import { Discord, Slash, SlashOption } from 'discordx';
+import Translate from '../../utils/translate';
+import { SteamService } from '../../services/steam.service';
+import { PlayerSummary, UserBans } from '../../types/steam';
+
+@Discord()
+export abstract class Steam {
+  private steamService: SteamService;
+
+  constructor() {
+    this.steamService = new SteamService();
+  }
+
+  /**
+   * Create Embed Message
+   * @param playerSummary
+   * @param userBans
+   * @returns MessageEmbed
+   */
+  private createMessage(
+    playerSummary: PlayerSummary,
+    userBans: UserBans
+  ): MessageEmbed {
+    return new MessageEmbed()
+      .setColor(0x0099ff)
+      .setAuthor(Translate.find('steamAuthor'), playerSummary?.avatarFull)
+      .setTitle(playerSummary.name ?? '~')
+      .setURL(playerSummary?.profileUrl ?? '')
+      .setThumbnail(playerSummary.avatarFull)
+      .setDescription(
+        Translate.find(
+          'steamDes',
+          playerSummary.realName ?? '~',
+          playerSummary.nameState ?? '~',
+          playerSummary.location?.cityName ?? '~',
+          playerSummary.location?.countryCode ?? '~',
+          playerSummary.timeCreated?.format('DD/MM/YYYY') ?? '~',
+          userBans.numberOfVACBans.toString() ?? '~',
+          userBans.numberOfGameBans.toString() ?? '~',
+          playerSummary.profileUrl as string
+        )
+      )
+      .setTimestamp();
+  }
+
+  @Slash('steam', {
+    description: 'Check and share your profile with friends on steam',
+  })
+  async init(
+    @SlashOption('name', {
+      description: 'Vanity Url?',
+      required: true,
+    })
+    url: string,
+    interaction: CommandInteraction
+  ): Promise<void> {
+    const user = await this.steamService.getVanityUser(url);
+
+    if (!user.steamId) {
+      await interaction.reply('**No valid steam vanity url was given!**');
+      await new Promise((resolve) => setTimeout(resolve, 5000));
+      return interaction.deleteReply();
+    }
+
+    const playerSummary = await this.steamService.getPlayerSummary(
+      user.steamId
+    );
+    const userBans = await this.steamService.getUserBans(user.steamId);
+
+    const msg = this.createMessage(playerSummary, userBans);
+    return interaction.reply({ embeds: [msg] });
+  }
+}
