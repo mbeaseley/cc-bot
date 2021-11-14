be3b163e6f143d0922dc0d44710c7eb484dd6642
mbeaseley <m.t.beaseley@gmail.com>
2021-11-14 11:53:31 +0000

    coin flip rewrite

---
+++ src/commands/fun/flip.ts
@@ -0,0 +1,75 @@
+import { APIMessage } from '@discordjs/builders/node_modules/discord-api-types/payloads/v9';
+import {
+  ButtonInteraction,
+  CommandInteraction,
+  MessageButton,
+  MessageActionRow,
+  Message,
+} from 'discord.js';
+import { ButtonComponent, Discord, Slash } from 'discordx';
+
+type Coin = 'Heads' | 'Tails';
+
+@Discord()
+export abstract class CoinFlip {
+  /**
+   * Create message
+   * @returns
+   */
+  createMessage(guess: Coin, result: Coin): string {
+    const guessResult = `${result === guess ? 'You win!' : 'You lose!'}`;
+    return `**${result}, ${guessResult} :coin:**`;
+  }
+
+  /**
+   * Coin flip Command
+   * @param interaction
+   */
+  @Slash('flip')
+  async init(
+    interaction: CommandInteraction
+  ): Promise<Message<true> | APIMessage | Message<boolean>> {
+    await interaction.deferReply();
+
+    const headsBtn = new MessageButton()
+      .setLabel('Heads')
+      .setEmoji('🪙')
+      .setStyle('PRIMARY')
+      .setCustomId('heads-btn');
+
+    const tailsBtn = new MessageButton()
+      .setLabel('Tails')
+      .setEmoji('🪙')
+      .setStyle('SECONDARY')
+      .setCustomId('tails-btn');
+
+    const row = new MessageActionRow().addComponents([headsBtn, tailsBtn]);
+
+    return interaction.editReply({
+      content: `Heads or Tails?`,
+      components: [row],
+    });
+  }
+
+  /**
+   * Heads Button Interaction
+   * @param interaction
+   */
+  @ButtonComponent('heads-btn')
+  headsAction(interaction: ButtonInteraction): Promise<void> {
+    const coinFlip = Math.floor(Math.random() * 2) === 0 ? 'Heads' : 'Tails';
+    const res = this.createMessage('Heads', coinFlip);
+    return interaction.reply(res);
+  }
+
+  /**
+   * Tails Button Interaction
+   * @param interaction
+   */
+  @ButtonComponent('tails-btn')
+  tailsAction(interaction: ButtonInteraction): Promise<void> {
+    const coinFlip = Math.floor(Math.random() * 2) === 0 ? 'Heads' : 'Tails';
+    const res = this.createMessage('Tails', coinFlip);
+    return interaction.reply(res);
+  }
+}
