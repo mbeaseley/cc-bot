3685209aa02a15d4002b9833129a758abec4e8a1
mbeaseley <m.t.beaseley@gmail.com>
2021-11-16 12:14:25 +0000

    choose player command rewrite

---
+++ src/commands/games/choose-player.ts
@@ -0,0 +1,154 @@
+import { APIMessage } from '@discordjs/builders/node_modules/discord-api-types/payloads/v9';
+import {
+  ButtonInteraction,
+  ClientUser,
+  Collection,
+  CommandInteraction,
+  GuildMember,
+  Message,
+  MessageActionRow,
+  MessageButton,
+  MessageEmbed,
+  User,
+} from 'discord.js';
+import { ButtonComponent, Discord, Slash } from 'discordx';
+import Translate from '../../utils/translate';
+
+@Discord()
+export abstract class ChoosePlayer {
+  private currentUser: User | undefined;
+  private previousInteraction:
+    | CommandInteraction
+    | ButtonInteraction
+    | undefined;
+
+  // /**
+  //  * Get exclude user id list
+  //  * @param command
+  //  */
+  // private getExcludeUsers(command: CommandMessage): string[] {
+  //   const commandContent = Utility.getOptionFromCommand(
+  //     command.content,
+  //     2
+  //   ) as string[];
+  //   return commandContent.map((c) => c.replace(/^[0-9]*$/, ''));
+  // }
+
+  /**
+   * Find random user from voice channel
+   * @param members
+   * @returns User
+   */
+  findRandomUser(members: Collection<string, GuildMember>): User | undefined {
+    const u = members
+      .map((m) => m.user)
+      .filter((user) => user.id !== undefined);
+    const previousUser = this.currentUser;
+
+    if (u?.length === 1) {
+      return u[0];
+    }
+
+    while (this.currentUser?.id === previousUser?.id) {
+      this.currentUser = u[Math.floor(Math.random() * u.length)] as User;
+    }
+
+    return this.currentUser as User;
+  }
+
+  /**
+   * Create message
+   * @param content
+   */
+  private createMessage(selectedUser: User): MessageEmbed {
+    return new MessageEmbed()
+      .setColor(selectedUser.hexAccentColor ?? 'RANDOM')
+      .setDescription(
+        Translate.find('playerChoiceDescription', selectedUser.username)
+      );
+  }
+
+  /**
+   * Interaction events
+   * @param interaction
+   * @param users
+   * @param msg
+   */
+  async onInteractionEvent(
+    interaction: CommandInteraction | ButtonInteraction,
+    users: Collection<string, GuildMember>,
+    msg: MessageEmbed
+  ): Promise<Message<true> | APIMessage | Message<boolean> | void> {
+    if (users.size > 1) {
+      await interaction.deferReply();
+
+      const rollAgainBtn = new MessageButton()
+        .setLabel('Someone else?')
+        .setEmoji('🎲')
+        .setStyle('SECONDARY')
+        .setCustomId('roll-again-btn');
+
+      const row = new MessageActionRow().addComponents([rollAgainBtn]);
+
+      return interaction.editReply({
+        embeds: [msg],
+        components: [row],
+      });
+    } else {
+      return interaction.reply({ embeds: [msg] });
+    }
+  }
+
+  /**
+   * Choose player command
+   * @param interaction
+   */
+  @Slash('choose-player', { description: 'Chooses Player in voice chat' })
+  async init(
+    interaction: CommandInteraction | ButtonInteraction
+  ): Promise<Message<true> | APIMessage | Message<boolean> | void> {
+    this.previousInteraction = interaction;
+    let users = await interaction.guild?.members.fetch();
+    let user = users?.find((u) => u.id === interaction.member.user.id);
+
+    if (!user) {
+      await interaction.reply('**Sorry, failed to find user!**');
+      await new Promise((resolve) => setTimeout(resolve, 5000));
+      return interaction.deleteReply();
+    }
+
+    const channel = user?.voice.channel;
+
+    if (!channel) {
+      await interaction.reply(
+        '**Please join voice channel to use this command!**'
+      );
+      await new Promise((resolve) => setTimeout(resolve, 5000));
+      return interaction.deleteReply();
+    }
+
+    const channelUsers = channel?.members.filter((m) => !m.user.bot);
+    const selectedUser = this.findRandomUser(channelUsers);
+
+    if (!selectedUser) {
+      await interaction.reply('**Sorry, failed to select user!**');
+      await new Promise((resolve) => setTimeout(resolve, 5000));
+      return interaction.deleteReply();
+    }
+
+    const msg = this.createMessage(selectedUser);
+    return this.onInteractionEvent(interaction, channelUsers, msg);
+  }
+
+  /**
+   * Heads Button Interaction
+   * @param interaction
+   */
+  @ButtonComponent('roll-again-btn')
+  async headsAction(
+    interaction: ButtonInteraction
+  ): Promise<Message<true> | APIMessage | Message<boolean> | void> {
+    await this.previousInteraction?.deleteReply();
+    return this.init(interaction);
+  }
+}
